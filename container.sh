4013acf02245372b146f07c65120be523098a051
simonkassel <kassel.simon@gmail.com>
2018-01-29 13:45:58 -0500

    update conatiner script

--- container.sh
+++ container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-xdg-open http://localhost:8000/
+
+mkdir -p $HOME/.aws
 
 docker run -it --rm --name geopyspark \
 	-p 8000:8000 -p 4040:4040 \
 	-v $(pwd $1):/home/hadoop/notebooks:rw \
-	-v $HOME.aws:/home/hadoop/.aws:ro \
+	-v $HOME/.aws:/home/hadoop/.aws:ro \
 	quay.io/geodocker/jupyter-geopyspark:6fe3aa0
