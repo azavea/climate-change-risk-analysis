a6b3b13c95bee0c3d6be94818ecc71f06f8df4a4
simonkassel <kassel.simon@gmail.com>
2018-01-31 11:58:54 -0500

    fix notebook

--- overlay/data/data.py
+++ overlay/data/data.py
@@ -3,6 +3,14 @@ from shapely.geometry import MultiPoint, MultiPolygon, shape
 from functools import partial
 import pyproj
 from shapely.ops import transform
+from urllib import request
+import zipfile
+
+
+def sea_level_rise(url, file):
+    request.urlretrieve(url, file)
+    z = zipfile.ZipFile(file)
+    z.extractall(file.replace('.zip', ''))
 
 
 def read_point_shp(shp):
