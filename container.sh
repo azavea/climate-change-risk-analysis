43eb4ecfc861eb8f70c578ea5ebf2595728fd81d
simonkassel <kassel.simon@gmail.com>
2018-02-22 18:12:28 -0500

    write permissions

--- container.sh
+++ container.sh
@@ -3,9 +3,14 @@
 # create local aws directory
 mkdir -p $HOME/.aws
 
+chmod o+w *.ipynb
+chmod o+w data/write/*
+chmod o+w data/output
+
 # run container
 docker run -it --rm --name geopyspark \
 	-p 8000:8000 -p 4040:4040 \
 	-v $(pwd $1):/home/hadoop/notebooks:rw \
 	-v $HOME/.aws:/home/hadoop/.aws:ro \
 	quay.io/geodocker/jupyter-geopyspark:6fe3aa0
+
